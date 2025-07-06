d74e4cd15873657e39a410abf4ae708f4b3d6e46
Muliika <samuelmuliika@gmail.com>
2025-07-06 19:57:32 +0300

    first commit

---
+++ variables.py
@@ -0,0 +1,9 @@
+# 50-python-concepts-every-developer-should-know-_-Book
+
+x = 5
+y = 3.4
+name = "john"
+my_list = [1, 2, 3]
+my_tuple = (1, 2, 3)
+my_set = {1, 2, 3}
+my_dict = {"name": "john", "age": 30}
