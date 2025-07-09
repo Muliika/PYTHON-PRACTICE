cd52ef49ba6e4397891348a7120fe315d1e40a57
Muliika <samuelmuliika@gmail.com>
2025-07-09 21:11:42 +0300

    an interesting lesson on modules and packages

---
+++ 005_modules_and_packages.py
@@ -0,0 +1,16 @@
+# Modules and packages are essential for organizing and managing python code, especially in larger projects.
+
+# A module is a file containing Python code. It can define functions, classes, and variables that can be imported and reused in other Python scripts.
+
+# Example 1
+import ex005my_module
+
+print(ex005my_module.greet("Alice"))
+
+
+# A package is a collection of modules organized in a directory hierarchy. It allows for better organization of code and can include sub-packages.
+# Example 2
+import ex005my_package
+
+ex005my_package.func1()
+ex005my_package.func2()
