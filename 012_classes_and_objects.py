85880aa39b5be5dbb73605d83864626769114c92
Muliika <samuelmuliika@gmail.com>
2025-07-16 21:25:34 +0300

    a refresher on classes 101

---
+++ 012_classes_and_objects.py
@@ -0,0 +1,3 @@
+# classes and objects are fundamental concepts in object-oriented programming (OOP). They allow us to create custom data structures and define their behavior. They allow you model real-world entities as objects with attributes(data) and methods(behavior).
+
+# A class is a blueprint/template for creating objects. It defines a set of attributes and methods that the objects created from the class will have. An object is an instance of a class, meaning it is created based on the class definition.
